818c8bdde07ee2b25494279a6730af4010683d37
oscardenas <oscardenas17@gmail.com>
2021-04-26 22:21:28 -0500

    Creando los archivos para las acciones CRUD

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php 
+    require '../includes/funciones.php';
+   
+    // include 'includes/templates/header.php';
+    incluirTemplate('header');
+
+
+?>
+
+    <main class="contenedor seccion">
+        <h1>Administrador de Bienes Raices</h1>
+
+
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+    </main>
+
+ 
+    <?php
+        incluirTemplate('footer');
+     ?>
---
+++ admin/propiedades/actualizar.php
@@ -0,0 +1,17 @@
+<?php 
+    require '../../includes/funciones.php';
+   
+    // include 'includes/templates/header.php';
+    incluirTemplate('header');
+
+
+?>
+
+    <main class="contenedor seccion">
+        <h1>Actualizar</h1>
+    </main>
+
+ 
+    <?php
+        incluirTemplate('footer');
+     ?>
